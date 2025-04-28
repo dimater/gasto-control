34115c95b3de6cd1aa17a4eb8577707d2a957f0c
Dimater <dimater@gmail.com>
2025-04-27 20:19:25 -0500

    Inicializar la variable de sesión 'user_logged_in' en index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 $routes = include __DIR__ . '/router.php'; // Ensure 'routes.php' returns an array of routes
 
 
-
+$_SESSION['user_logged_in'] = true; // Initialize session variable for user login status
 // Check if user is logged in
 if (!isset($_SESSION['user_logged_in']) || $_SESSION['user_logged_in'] !== true) {
     // Redirect to login page if not already on it
